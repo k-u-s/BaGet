87ebc2b8668d953970e45deb775ad873cb53a055
Kus <git@kus.dev>
2021-04-19 14:15:54 +0200

    Added policy name to BaGetApi

--- src/BaGet.Hosting/BaGetApi.cs
+++ src/BaGet.Hosting/BaGetApi.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text;
 using BaGet.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
@@ -7,6 +9,17 @@ namespace BaGet
 {
     public class BaGetApi
     {
+        private readonly string _policyName;
+
+        private string[] _policyNames => string.IsNullOrWhiteSpace(_policyName)
+            ? Array.Empty<string>()
+            : new[] {_policyName};
+
+        public BaGetApi(string policyName = default)
+        {
+            _policyName = policyName;
+        }
+
         public void MapRoutes(IEndpointRouteBuilder endpoints)
         {
             MapServiceIndexRoutes(endpoints);
@@ -22,7 +35,8 @@ namespace BaGet
             endpoints.MapControllerRoute(
                 name: Routes.IndexRouteName,
                 pattern: "v3/index.json",
-                defaults: new { controller = "ServiceIndex", action = "Get" });
+                defaults: new { controller = "ServiceIndex", action = "Get" })
+                .RequireAuthorization(_policyNames);
         }
 
         public void MapPackagePublishRoutes(IEndpointRouteBuilder endpoints)
@@ -31,19 +45,22 @@ namespace BaGet
                 name: Routes.UploadPackageRouteName,
                 pattern: "api/v2/package",
                 defaults: new { controller = "PackagePublish", action = "Upload" },
-                constraints: new { httpMethod = new HttpMethodRouteConstraint("PUT") });
+                constraints: new { httpMethod = new HttpMethodRouteConstraint("PUT") })
+                .RequireAuthorization(_policyNames);
 
             endpoints.MapControllerRoute(
                 name: Routes.DeleteRouteName,
                 pattern: "api/v2/package/{id}/{version}",
                 defaults: new { controller = "PackagePublish", action = "Delete" },
-                constraints: new { httpMethod = new HttpMethodRouteConstraint("DELETE") });
+                constraints: new { httpMethod = new HttpMethodRouteConstraint("DELETE") })
+                .RequireAuthorization(_policyNames);
 
             endpoints.MapControllerRoute(
                 name: Routes.RelistRouteName,
                 pattern: "api/v2/package/{id}/{version}",
                 defaults: new { controller = "PackagePublish", action = "Relist" },
-                constraints: new { httpMethod = new HttpMethodRouteConstraint("POST") });
+                constraints: new { httpMethod = new HttpMethodRouteConstraint("POST") })
+                .RequireAuthorization(_policyNames);
         }
 
         public void MapSymbolRoutes(IEndpointRouteBuilder endpoints)
@@ -52,17 +69,20 @@ namespace BaGet
                 name: Routes.UploadSymbolRouteName,
                 pattern: "api/v2/symbol",
                 defaults: new { controller = "Symbol", action = "Upload" },
-                constraints: new { httpMethod = new HttpMethodRouteConstraint("PUT") });
+                constraints: new { httpMethod = new HttpMethodRouteConstraint("PUT") })
+                .RequireAuthorization(_policyNames);
 
             endpoints.MapControllerRoute(
                 name: Routes.SymbolDownloadRouteName,
                 pattern: "api/download/symbols/{file}/{key}/{file2}",
-                defaults: new { controller = "Symbol", action = "Get" });
+                defaults: new { controller = "Symbol", action = "Get" })
+                .RequireAuthorization(_policyNames);
 
             endpoints.MapControllerRoute(
                 name: Routes.PrefixedSymbolDownloadRouteName,
                 pattern: "api/download/symbols/{prefix}/{file}/{key}/{file2}",
-                defaults: new { controller = "Symbol", action = "Get" });
+                defaults: new { controller = "Symbol", action = "Get" })
+                .RequireAuthorization(_policyNames);
         }
 
         public void MapSearchRoutes(IEndpointRouteBuilder endpoints)
@@ -70,18 +90,21 @@ namespace BaGet
             endpoints.MapControllerRoute(
                 name: Routes.SearchRouteName,
                 pattern: "v3/search",
-                defaults: new { controller = "Search", action = "Search" });
+                defaults: new { controller = "Search", action = "Search" })
+                .RequireAuthorization(_policyNames);
 
             endpoints.MapControllerRoute(
                 name: Routes.AutocompleteRouteName,
                 pattern: "v3/autocomplete",
-                defaults: new { controller = "Search", action = "Autocomplete" });
+                defaults: new { controller = "Search", action = "Autocomplete" })
+                .RequireAuthorization(_policyNames);
 
             // This is an unofficial API to find packages that depend on a given package.
             endpoints.MapControllerRoute(
                 name: Routes.DependentsRouteName,
                 pattern: "v3/dependents",
-                defaults: new { controller = "Search", action = "Dependents" });
+                defaults: new { controller = "Search", action = "Dependents" })
+                .RequireAuthorization(_policyNames);
         }
 
         public void MapPackageMetadataRoutes(IEndpointRouteBuilder endpoints)
@@ -89,12 +112,14 @@ namespace BaGet
             endpoints.MapControllerRoute(
                name: Routes.RegistrationIndexRouteName,
                pattern: "v3/registration/{id}/index.json",
-               defaults: new { controller = "PackageMetadata", action = "RegistrationIndex" });
+               defaults: new { controller = "PackageMetadata", action = "RegistrationIndex" })
+                .RequireAuthorization(_policyNames);
 
             endpoints.MapControllerRoute(
                 name: Routes.RegistrationLeafRouteName,
                 pattern: "v3/registration/{id}/{version}.json",
-                defaults: new { controller = "PackageMetadata", action = "RegistrationLeaf" });
+                defaults: new { controller = "PackageMetadata", action = "RegistrationLeaf" })
+                .RequireAuthorization(_policyNames);
         }
 
         public void MapPackageContentRoutes(IEndpointRouteBuilder endpoints)
@@ -102,27 +127,32 @@ namespace BaGet
             endpoints.MapControllerRoute(
                 name: Routes.PackageVersionsRouteName,
                 pattern: "v3/package/{id}/index.json",
-                defaults: new { controller = "PackageContent", action = "GetPackageVersions" });
+                defaults: new { controller = "PackageContent", action = "GetPackageVersions" })
+                .RequireAuthorization(_policyNames);
 
             endpoints.MapControllerRoute(
                 name: Routes.PackageDownloadRouteName,
                 pattern: "v3/package/{id}/{version}/{idVersion}.nupkg",
-                defaults: new { controller = "PackageContent", action = "DownloadPackage" });
+                defaults: new { controller = "PackageContent", action = "DownloadPackage" })
+                .RequireAuthorization(_policyNames);
 
             endpoints.MapControllerRoute(
                 name: Routes.PackageDownloadManifestRouteName,
                 pattern: "v3/package/{id}/{version}/{id2}.nuspec",
-                defaults: new { controller = "PackageContent", action = "DownloadNuspec" });
+                defaults: new { controller = "PackageContent", action = "DownloadNuspec" })
+                .RequireAuthorization(_policyNames);
 
             endpoints.MapControllerRoute(
                 name: Routes.PackageDownloadReadmeRouteName,
                 pattern: "v3/package/{id}/{version}/readme",
-                defaults: new { controller = "PackageContent", action = "DownloadReadme" });
+                defaults: new { controller = "PackageContent", action = "DownloadReadme" })
+                .RequireAuthorization(_policyNames);
 
             endpoints.MapControllerRoute(
                 name: Routes.PackageDownloadIconRouteName,
                 pattern: "v3/package/{id}/{version}/icon",
-                defaults: new { controller = "PackageContent", action = "DownloadIcon" });
+                defaults: new { controller = "PackageContent", action = "DownloadIcon" })
+                .RequireAuthorization(_policyNames);
         }
     }
 }
